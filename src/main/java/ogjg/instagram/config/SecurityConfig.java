11dc0a112e3de3cda7e9a8488c59466aa71ce756
ByeongGyu An <devafox62@gmail.com>
2023-08-11 12:42:55 +0900

    PasswordEncoder() 추가

--- src/main/java/ogjg/instagram/config/SecurityConfig.java
+++ src/main/java/ogjg/instagram/config/SecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -55,5 +57,10 @@ public class SecurityConfig {
         source.registerCorsConfiguration("**", configuration);
 
         return source;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
