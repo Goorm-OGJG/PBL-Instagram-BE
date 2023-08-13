9d73b77c811fd8d173f7d7c7c4d683195530e974
ByeongGyu An <devafox62@gmail.com>
2023-08-13 14:27:50 +0900

    :sparkles: Refresh Token 저장을 위한 Entity, Repository 구현

---
+++ src/main/java/ogjg/instagram/user/domain/UserAuthentication.java
@@ -0,0 +1,37 @@
+package ogjg.instagram.user.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = PROTECTED)
+public class UserAuthentication {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String nickname;
+
+    @Column(unique = true)
+    private String refreshToken;
+
+    @Builder
+    public UserAuthentication(Long id, String username, String nickname, String refreshToken) {
+        this.id = id;
+        this.username = username;
+        this.nickname = nickname;
+        this.refreshToken = refreshToken;
+    }
+
+    public void updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
