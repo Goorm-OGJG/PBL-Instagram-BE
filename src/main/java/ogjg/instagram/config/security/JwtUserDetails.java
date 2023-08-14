7ac61091b67f1c22822629036d7afdd72ac1bb90
ByeongGyu An <devafox62@gmail.com>
2023-08-14 14:45:24 +0900

    :sparkles: JWT 인증 완료시 정보 조회를 위한 JwtUserDetails 구현

---
+++ src/main/java/ogjg/instagram/config/security/JwtUserDetails.java
@@ -0,0 +1,58 @@
+package ogjg.instagram.config.security;
+
+import lombok.RequiredArgsConstructor;
+import ogjg.instagram.user.dto.JwtUserClaimsDto;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class JwtUserDetails implements UserDetails {
+
+    private final JwtUserClaimsDto jwtUserClaimsDto;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    public Long getUserId() {
+        return jwtUserClaimsDto.getUserId();
+    }
+
+    @Override
+    public String getUsername() {
+        return jwtUserClaimsDto.getUsername();
+    }
+
+    public String getNickname() {
+        return jwtUserClaimsDto.getNickname();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
