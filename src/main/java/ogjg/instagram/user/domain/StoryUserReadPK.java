94dd4f2605ba71fbcda5d97c4f98b041f92502d4
DongJinLee <dlehdwls21@naver.com>
2023-08-14 22:15:47 +0900

    :sparkles: StoryUserRead 생성자,StoryUserReadPK 생성자 추가

--- src/main/java/ogjg/instagram/user/domain/StoryUserReadPK.java
+++ src/main/java/ogjg/instagram/user/domain/StoryUserReadPK.java
@@ -1,13 +1,22 @@
 package ogjg.instagram.user.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import ogjg.instagram.story.dto.request.StoryUserReadDto;
 
 import java.io.Serializable;
 
 @Embeddable
 @EqualsAndHashCode
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class StoryUserReadPK implements Serializable {
     private Long userId;
     private Long storyId;
+
+    public StoryUserReadPK(StoryUserReadDto storyUserReadDto) {
+        this.userId = storyUserReadDto.getUserId();
+        this.storyId = storyUserReadDto.getStoryId();
+    }
 }
