958da85f0787cc89f00610c1be3434b227906d44
dunowljj <uuufg3322@gmail.com>
2023-08-19 17:53:42 +0900

    :bug: feedMedia save 오류 수정

---
+++ src/main/java/ogjg/instagram/feed/repository/FeedMediaRepository.java
@@ -0,0 +1,8 @@
+package ogjg.instagram.feed.repository;
+
+import ogjg.instagram.feed.domain.FeedMedia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedMediaRepository extends JpaRepository<FeedMedia, Long> {
+
+}
