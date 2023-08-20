0407c54577e1aac99c01d064e330cf083db9a614
dunowljj <uuufg3322@gmail.com>
2023-08-21 00:15:53 +0900

    :bug: innerComment 목록보기 기능 누락된 부분 추가
    - createdAt 추가
    - likeStatus 추가
    - likeCount 추가
    - save createAt 누락 추가

--- src/main/java/ogjg/instagram/comment/service/InnerCommentService.java
+++ src/main/java/ogjg/instagram/comment/service/InnerCommentService.java
@@ -4,13 +4,17 @@ import lombok.RequiredArgsConstructor;
 import ogjg.instagram.comment.domain.Comment;
 import ogjg.instagram.comment.domain.InnerComment;
 import ogjg.instagram.comment.dto.request.InnerCommentCreateRequestDto;
+import ogjg.instagram.comment.dto.response.InnerCommentListResponseDto;
 import ogjg.instagram.comment.repository.InnerCommentRepository;
+import ogjg.instagram.like.service.InnerCommentLikeService;
 import ogjg.instagram.user.domain.User;
 import ogjg.instagram.user.service.UserService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -18,6 +22,7 @@ public class InnerCommentService {
     private final UserService userService;
     private final CommentService commentService;
     private final InnerCommentRepository innerCommentRepository;
+    private final InnerCommentLikeService innerCommentLikeService;
 
     @Transactional
     public void write(Long userId, Long commentId, InnerCommentCreateRequestDto requestDto) {
@@ -52,5 +57,26 @@ public class InnerCommentService {
     public InnerComment findById(Long innerCommentId) {
         return innerCommentRepository.findById(innerCommentId)
                 .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 답글입니다. id =" + innerCommentId));
+    }
+
+    @Transactional(readOnly = true)
+    public InnerCommentListResponseDto innerCommentList(Long commentId, Long userId) {
+        return InnerCommentListResponseDto.builder()
+                .commentId(commentId)
+                .innerComments(
+                        commentService.findInnerComments(commentId).stream()
+                                .map(toInnerCommentListDto(userId))
+                                .collect(Collectors.toUnmodifiableList())
+                )
+                .build();
+    }
+
+    private Function<InnerComment, InnerCommentListResponseDto.InnerCommentListDto> toInnerCommentListDto(Long userId) {
+        return innerComment ->
+                InnerCommentListResponseDto.InnerCommentListDto.from(
+                        innerComment,
+                        innerCommentLikeService.likeStatus(userId, innerComment.getId()),
+                        innerCommentLikeService.innerCommentLikeCount(innerComment.getId())
+                );
     }
 }
