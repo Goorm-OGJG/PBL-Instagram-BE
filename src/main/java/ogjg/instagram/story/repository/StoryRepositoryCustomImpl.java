f4575a0d4d988af031cdab9d4eb4867bd6796200
dunowljj <uuufg3322@gmail.com>
2023-08-21 15:04:53 +0900

    story 시간 조건 및 정렬 추가

--- src/main/java/ogjg/instagram/story/repository/StoryRepositoryCustomImpl.java
+++ src/main/java/ogjg/instagram/story/repository/StoryRepositoryCustomImpl.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import ogjg.instagram.story.dto.response.QStoryListDto;
 import ogjg.instagram.story.dto.response.StoryListDto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static ogjg.instagram.story.domain.QStory.story;
@@ -28,6 +29,8 @@ public class StoryRepositoryCustomImpl implements StoryRepositoryCustom{
                 )).from(story)
                 .join(user)
                 .on(story.user.id.eq(user.id))
+                .where(story.createdAt.between(LocalDateTime.now().minusHours(24),LocalDateTime.now()))
+                .orderBy(story.createdAt.desc())
                 .fetch();
 
         JPAQuery<Long> storyCount = jpaQueryFactory.select(story
