9386b7a3085a958646e192b4b160397a37bed756
dunowljj <92416661+dunowljj@users.noreply.github.com>
2023-08-22 23:10:05 +0900

    :bug: 댓글 createAt 누락 추가 (#79)

--- src/main/java/ogjg/instagram/comment/domain/Comment.java
+++ src/main/java/ogjg/instagram/comment/domain/Comment.java
@@ -58,6 +58,7 @@ public class Comment {
                 .user(User.reference(loginId))
                 .feed(Feed.reference(feedId))
                 .content(content)
+                .createdAt(LocalDateTime.now())
                 .build();
     }
 }
