d963ae1d89f8840d7b24648678f3307b28f2b00e
ByeongGyu An <devafox62@gmail.com>
2023-08-25 12:36:54 +0900

    :hammer: 인증번호를 사용한 DB 데이터 조회 메서드 추가

--- src/main/java/ogjg/instagram/user/repository/UserAuthenticationNumberRepository.java
+++ src/main/java/ogjg/instagram/user/repository/UserAuthenticationNumberRepository.java
@@ -3,5 +3,8 @@ package ogjg.instagram.user.repository;
 import ogjg.instagram.user.domain.UserAuthenticationNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserAuthenticationNumberRepository extends JpaRepository<UserAuthenticationNumber, Long> {
+    Optional<UserAuthenticationNumber> findByAuthenticationCode(String authenticationCode);
 }
