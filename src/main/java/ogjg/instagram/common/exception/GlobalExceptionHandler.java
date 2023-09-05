55b9cb84e33fd12225be6a41ca845b79f9357974
ByeongGyu An <devafox62@gmail.com>
2023-09-06 00:38:36 +0900

    :bug: 토큰 재발급 로직 글로벌 예외 처리 (#135)

--- src/main/java/ogjg/instagram/common/exception/GlobalExceptionHandler.java
+++ src/main/java/ogjg/instagram/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ogjg.instagram.common.exception;
 
+import io.jsonwebtoken.JwtException;
 import lombok.extern.slf4j.Slf4j;
 import ogjg.instagram.common.dto.ErrorResponseDto;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,6 +20,16 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleIllegalArgumentException(WebRequest request, IllegalArgumentException e) {
         log.error("IllegalArgumentException {}", e.getMessage());
         ErrorResponseDto response = new ErrorResponseDto(HttpStatus.BAD_REQUEST, e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = JwtException.class)
+    public ResponseEntity<?> handleJwtException(WebRequest request, Exception e) {
+        log.error("[Exception] {}", e.getMessage());
+        HashMap<String, String> response = new HashMap<>(Map.of(
+                "errorType", "INVALID_REFRESH_TOKEN",
+                "message", e.getMessage()
+        ));
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
