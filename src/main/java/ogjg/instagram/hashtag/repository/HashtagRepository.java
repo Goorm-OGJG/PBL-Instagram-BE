dfbf94fd78cba2d542bf1c5903574840df8e3fc3
dunowljj <uuufg3322@gmail.com>
2023-09-05 15:38:10 +0900

    변경 전 내용 삭제

--- src/main/java/ogjg/instagram/hashtag/repository/HashtagRepository.java
+++ src/main/java/ogjg/instagram/hashtag/repository/HashtagRepository.java
@@ -1,17 +1,7 @@
 package ogjg.instagram.hashtag.repository;
 
 import ogjg.instagram.hashtag.domain.Hashtag;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
-    @Query("""
-            SELECT h.content FROM Hashtag h
-            join h.hashtagFeeds hf WHERE h.content LIKE :searchKey
-            group by h.content
-            """)
-    Page<String> findByContentsContaining(@Param("searchKey") String searchKey, Pageable pageable);
 }
